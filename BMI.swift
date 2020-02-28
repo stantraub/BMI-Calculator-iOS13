2a8732bda1776acd80f0e92eee43f2ef779bc57e
Stanley <stanley.traub@gmail.com>
2020-02-28 13:40:56 -0800

    Finished BMI app

---
+++ BMI.swift
@@ -0,0 +1,17 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Stanley Traub on 2/28/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+    
+    
+}
